8dcc19c5546ae5554744ff888f361ad3a89a64c6
samPuriwat <sam.puriwat@gmail.com>
2020-07-17 13:44:53 +0700

    add more code

--- src/Lab1/HelloWorld.java
+++ src/Lab1/HelloWorld.java
@@ -5,6 +5,9 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         System.out.println("Hello World !!!");
+        System.out.println("Puriwat Lertkrai");
+        System.out.println("Management Information Technology");
+        System.out.println("RUTS");
     }
 
 }

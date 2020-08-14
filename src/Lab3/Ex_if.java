c48f9fc2e867d5b1523a6706f31ca886acb4f701
samPuriwat <sam.puriwat@gmail.com>
2020-08-14 16:10:01 +0700

    Exercise Lab3

---
+++ src/Lab3/Ex_if.java
@@ -0,0 +1,19 @@
+package Lab3;
+
+public class Ex_if {
+
+    public static void main(String[] args) {
+
+        int x = 100;
+        int y = 20;
+
+        if (x<y)
+            System.out.println(x + " less than " + y);
+        System.out.println("Hello");
+        System.out.println("Good Bye.");
+
+
+
+
+    }
+}
